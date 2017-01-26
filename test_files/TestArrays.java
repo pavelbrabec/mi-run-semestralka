3454e1f9ab06dc7ecd5042af87c7adc08af8ee78
Pavel Brabec <brabepa1@fit.cvut.cz>
2017-01-26 21:16:34 +0100

    Prepared TestArrays

---
+++ test_files/TestArrays.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author pavel
+ */
+public class TestArrays {
+    
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[3]=3;
+        int x = a[3];
+    }
+
+}
