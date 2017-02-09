0433ed2a19201b7b4eff380a5510ed19e59921c2
Pavel Brabec <brabepa1@fit.cvut.cz>
2017-02-09 16:43:20 +0100

    Mockito dependency

---
+++ src/test/java/cz/cvut/fit/brabepa1/run/interpret/instructions/impl/math/IaddTest.java
@@ -0,0 +1,19 @@
+
+package cz.cvut.fit.brabepa1.run.interpret.instructions.impl.math;
+
+import org.junit.Test;
+/**
+ *
+ * @author pavel
+ */
+public class IaddTest {
+    
+    public IaddTest() {
+    }
+
+    @Test
+    public void testExecute() {
+        
+    }
+    
+}
